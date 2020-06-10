8dc37e74ebcaed3e8a6b47585418b640060d76e9
Natalia <nkirpa.git@gmail.com>
2020-06-10 15:35:34 +0400

    added toggle bar with list item

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("h1").innerHTML = "Hello Universe!";
+window.toggleLeft = function() {
+   var element = document.getElementById("offcanvas-left");
+   element.classList.toggle("hide");
+}
+window.toggleRight = function() {
+   var element = document.getElementById("offcanvas-right");
+   element.classList.toggle("hide");
+}
